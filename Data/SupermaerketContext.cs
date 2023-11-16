4c2ce9f8b8ddcfd8c2150c54b952f0e7c645bc3a
Jose Manuel Gonzalez Trochez <jgonzalez7243@campuscecep.edu.co>
2023-11-15 22:07:02 -0500

    Agregar archivos de proyecto.

---
+++ Data/SupermaerketContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SupermarketEF.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SupermarketEF.Data
+{
+    internal class SupermaerketContext: DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+		public DbSet<Provider> Providers { get; set; }
+
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=SupermarketEF;Trusted_Connection=True;");
+        }
+    }
+    
+}
---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using SupermarketEF.Data;
+using SupermarketEF.Models;
+
+namespace SupermarketEF
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using SupermaerketContext context = new SupermaerketContext();
+            //se crea el objeto oilcategory de tipo category
+            Category oilCategory = new Category()
+            {
+                Name = "Oil",
+                Description = "Oil Category"
+            };
+            //Se agrega el objeto creado al contexto de la BD 
+            //Usando la propieda Categories del Contexto
+            context.Categories.Add(oilCategory);
+            //Se crea el objeto grainsCategory de tipo Category
+            Category grainsCategory = new Category()
+            {
+                Name= "Grains",
+                Description = "Grains Category"
+            };
+            //Se agrega el objeto creado al contexto de la BD
+            //context.Add(grainsCategory);
+            //Se graban los cambios realizados al contexto
+            //context.SaveChanges();
+            
+            //Recupera todas las categorias y las ordena por el nombre
+            var grainCategory = context.Categories
+                .Where(p=>p.Name == "Grains")
+                .FirstOrDefault();
+            if(grainsCategory is Category)
+            {
+                grainCategory.Description = "New description applied";
+                context.SaveChanges();
+            }
+            
+
+            var categories = context.Categories.OrderBy(p=> p.Name );
+            foreach ( var category in categories )
+            {
+                Console.WriteLine($"{category.Name} | {category.Description}");
+            }
+
+		}
+    }
+}
